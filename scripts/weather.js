56ffcfdbd96b6ef1813eba5d03df428551d930bb
German Gamboa <germangamboa95@gmail.com>
2018-07-07 23:31:39 -0400

    refactored to use module pattern

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,68 +1,49 @@
-let info = 0;
+const UiCtrl = (function() {
+  const location = document.querySelector(".card-container");
 
-function getData(location, callback){
-    let lon =  location[0];
-    let lat = location[1];
-    let url = `http://api.wunderground.com/api/abd241a01af7a2a0/forecast/q/${lat},${lon}.json`;
-    const xhr = new XMLHttpRequest();
-    xhr.open('Get',url,true);
-    xhr.onload = function(){
-        console.log(xhr.status);
-        callback(JSON.parse(xhr.response));
-      }
-
-    xhr.send();
-  }
-
-
-function getLoc() {
-    let coords = 0;
-    navigator.geolocation.getCurrentPosition(function(pos){
-      let crd = pos.coords;
-      let lat = crd.latitude;
-      let lon = crd.longitude;
-      coords = [lon,lat];
-      getData(coords,results);
-    });
-    function results(res){
-      info = res;
+  const render = forecasts => {
+    for (let i = 0; i < forecasts.length; i = i + 2) {
+      let markUp = `
+          <div class="card zoomIn animated">
+            <h1 id='day'>${forecasts[i].title}</h1>
+            <img src="${forecasts[i].icon_url}">
+            <p id="forecast">
+              ${forecasts[i].fcttext}
+            </p>
+            <h1 id='day'>${forecasts[i + 1].title}</h1>
+            <img src="${forecasts[i + 1].icon_url}">
+            <p id="forecast">
+              ${forecasts[i + 1].fcttext}
+            </p>
+          </div>
+        `;
+      location.innerHTML += markUp;
     }
+  };
+  return {
+    render: render
+  };
+})();
+
+const mainCtrl = (async function(UiCtrl, getGeoData, getWeatherData) {
+  const locationData = await getGeoData();
+  const weatherData = await getWeatherData(locationData);
+  UiCtrl.render(weatherData);
+})(UiCtrl, getGeoData, getWeatherData);
+
+function getGeoData() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      let { latitude, longitude } = pos.coords;
+      resolve({ latitude, longitude });
+    });
+  });
 }
 
-
-  function createCard(forecasts){
-    for(let i = 0; i < forecasts.length; i = i + 2){
-      let card = document.createElement('div');
-      let markUp = `
-        <div class="card zoomIn animated">
-          <h1 id='day'>${forecasts[i].title}</h1>
-          <img src="${forecasts[i].icon_url}">
-          <p id="forecast">
-            ${forecasts[i].fcttext}
-          </p>
-          <h1 id='day'>${forecasts[i+1].title}</h1>
-          <img src="${forecasts[i+1].icon_url}">
-          <p id="forecast">
-            ${forecasts[i+1].fcttext}
-          </p>
-        </div>
-      `;
-      card.innerHTML = markUp;
-      let location = document.querySelector('.card-container');
-      location.appendChild(card);
-    }
-  }
-
-
-getLoc();
-
-let idk = setInterval(function(){
-  if(info===0){
-    console.log("waiting for info");}
-    else{
-      let forecasts = info.forecast.txt_forecast.forecastday;
-      console.log(forecasts);
-      clearInterval(idk);
-      createCard(forecasts);
-    }
-},100)
+async function getWeatherData({ longitude, latitude }) {
+  let url = `http://api.wunderground.com/api/abd241a01af7a2a0/forecast/q/${latitude},${longitude}.json`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const forecasts = data.forecast.txt_forecast.forecastday;
+  return forecasts;
+}
